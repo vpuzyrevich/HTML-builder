b6d53cb1d10a0a9e0c5c65ed0ace420bf3c1ea1e
vpuzyrevich <v.puzyrevich@gmail.com>
2022-05-19 10:38:44 +0300

    feat: task 01-read-file completed

---
+++ 01-read-file/index.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+const stream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+let data = '';
+stream.on('data', chunk => data += chunk);
+stream.on('end', () => console.log(data));
