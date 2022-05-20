8d9352dc61bfc0dd7bc40e062a83ae01aa4e8c3b
vpuzyrevich <v.puzyrevich@gmail.com>
2022-05-20 13:58:14 +0300

    feat: task 04 complated

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true },  err => {
+  if (err) {
+    return console.error(err);
+  }
+});
+fs.promises.readdir(path.join(__dirname, 'files-copy'))
+  .then(filenames => {
+    for (let filename of filenames) {
+      fs.promises.unlink(path.join(__dirname, 'files-copy', filename));
+    }});
+fs.promises.readdir(path.join(__dirname, 'files'))
+  .then(filenames => {
+    for (let filename of filenames) {
+      fs.promises.copyFile(path.join(__dirname, 'files', filename), path.join(__dirname, 'files-copy', filename));
+    }});
 
