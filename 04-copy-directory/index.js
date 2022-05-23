723a3445a3a463a7cfa7f57bf70873ef1547202f
vpuzyrevich <v.puzyrevich@gmail.com>
2022-05-24 00:24:04 +0300

    refactor: refactor task 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,19 +1,21 @@
 const fs = require('fs');
 const path = require('path');
+(async() => {
+  await fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true },  err => {
+    if (err) {
+      return console.error(err);
+    }
+  });
+  await fs.promises.readdir(path.join(__dirname, 'files-copy'))
+    .then(filenames => {
+      for (let filename of filenames) {
+        fs.promises.unlink(path.join(__dirname, 'files-copy', filename));
+      }});
+  await fs.promises.readdir(path.join(__dirname, 'files'))
+    .then(filenames => {
+      for (let filename of filenames) {
+        fs.promises.copyFile(path.join(__dirname, 'files', filename), path.join(__dirname, 'files-copy', filename));
+      }});
+}) ();
 
-fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true },  err => {
-  if (err) {
-    return console.error(err);
-  }
-});
-fs.promises.readdir(path.join(__dirname, 'files-copy'))
-  .then(filenames => {
-    for (let filename of filenames) {
-      fs.promises.unlink(path.join(__dirname, 'files-copy', filename));
-    }});
-fs.promises.readdir(path.join(__dirname, 'files'))
-  .then(filenames => {
-    for (let filename of filenames) {
-      fs.promises.copyFile(path.join(__dirname, 'files', filename), path.join(__dirname, 'files-copy', filename));
-    }});
 
